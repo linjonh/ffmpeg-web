995fce81d70d3e84f4b4a5b57fa7566285b3138e
user@zp-nc04 <user@zp-nc04>
2025-05-24 04:33:25 +0800

    多文件并发，单文件内部使用asyncio保持顺序

--- translate.py
+++ translate.py
@@ -10,7 +10,7 @@ import os
 import glob
 from bs4.element import NavigableString, PageElement
 from utils import timeCost
-
+import ast
 
 from azure_openai_cli import call_chatgpt
 
@@ -22,14 +22,14 @@ def contains_english(text: str):
 pool = ThreadPoolExecutor(max_workers=os.cpu_count())
 
 
-def append_task(task:list, text_array: dict[int, str]):
+def append_task(task: list, text_array: dict[int, str]):
     # return task.append(sync_api(prompt=f"翻译下数组里的文案，返回数组：{text_array}"))
     if len(text_array) == 0:
         return None
     print(f"翻译源数组大小：size{len(text_array)}")
-    json_str= json.dumps(text_array, ensure_ascii=False)
-    prompt = f"翻译下数json的values文案，直接返回翻译后的json，不要输出markdown，要纯json文本格式输出：{json_str}"
-    return task.append(call_chatgpt(prompt=prompt))
+    json_str = json.dumps(text_array, ensure_ascii=False).replace("'", "&apos;")
+    prompt = f"翻译下数json的values文案，直接返回翻译后的json，不要输出markdown，要纯json文本格式输出，转义的字符保持原样输出：{json_str}"
+    return task.append(call_chatgpt(prompt=prompt, is_data_json=True))
 
 
 async def handle_html_file(html: str, base_name: str):
@@ -37,10 +37,10 @@ async def handle_html_file(html: str, base_name: str):
     leng = 0
     chunk_size = 0
     task = []
-    text_array: dict[int,str] = {}
-    translate_arry:dict[int,str] = {}
+    text_array: dict[int, str] = {}
+    translate_arry: dict[int, str] = {}
     childs = soup.descendants
-    nodes: dict[int,PageElement] = {}
+    nodes: dict[int, PageElement] = {}
     for i, node in enumerate(childs):
         if (
             isinstance(node, NavigableString)
@@ -48,7 +48,15 @@ async def handle_html_file(html: str, base_name: str):
             and not isinstance(node, Doctype)
         ):
             parent = node.parent
-            if parent.name in ["script", "style", "noscript", "pre", "code", "samp", "template"]:
+            if parent.name in [
+                "script",
+                "style",
+                "noscript",
+                "pre",
+                "code",
+                "samp",
+                "template",
+            ]:
                 print("skip script, style, noscript ")
                 continue
 
@@ -71,18 +79,23 @@ async def handle_html_file(html: str, base_name: str):
 
     all_results = await asyncio.gather(*task)
     for i, rep in enumerate(all_results):
-        print(f"{i} rep={type(rep)}")
+        # json_str=rep.replace("\"","\\\"").replace("'","\"")
+        print(f"{i} {type(rep)} rep={rep}")
         if rep is None:
             print(f"{i} rep is None")
             continue
-        translate_arry.update(json.loads(rep))
+        # ast.literal_eval(rep)
+        translate_arry.update(rep)
     print(f"===> translate_arry={translate_arry}")
-    for i,node in nodes.items:
-        node:PageElement
-        print(f"before {node}")
-        text = translate_arry[i].strip()
-        node.replace_with(text)
-        print(f"after ====> {text}")
+    for i, node in nodes.items():
+        node: PageElement
+        print(f"【{i}】before {node}")
+        text = translate_arry.get(str(i), "").strip()
+        if text != "":
+            node.replace_with(text)
+            print(f"【{i}】after ====> {text}")
+        else:
+            print(f"【{i}】after ====> not tranlate")
     with open(f"{docs_path_cn}/{base_name}", "w", encoding="utf-8") as f:
         # Comment:
         string: str = str(soup)
@@ -118,6 +131,10 @@ if __name__ == "__main__":
         list.sort(list_files)
         length = 0
         str_length = 0
+        pool = ThreadPoolExecutor(
+            max_workers=os.cpu_count(), thread_name_prefix="trans_file_thread"
+        )
+        jobs = []
         for item in list_files:  # import os
             print(item)
             base_name = os.path.basename(item)
@@ -125,9 +142,11 @@ if __name__ == "__main__":
                 # Comment:
                 data = f.read()
                 length += len(data)
-                str_length += asyncio.run(handle_html_file(data, base_name))
-                print(
-                    f"{base_name:<30} read size={len(data):>12,}    total size={length:>12,} string_len={str_length:>12,}"
-                )
+                jobs.append(pool.submit(asyncio.run, handle_html_file(data, base_name)))
+        for job in futures.as_completed(jobs):
+            str_length += job.result()
+            print(
+                f"{base_name:<30} read size={len(data):>12,}    total size={length:>12,} string_len={str_length:>12,}"
+            )
 
     main()
